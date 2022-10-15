254c9bc0890a1e002525aeb249722124041a2921
Mykola Osadchii <nikolayosadchii842@gmail.com>
2022-10-15 18:55:33 +0300

    add goods card

--- js/catalog.js
+++ js/catalog.js
@@ -88,27 +88,4 @@ showMoreButton.addEventListener("click", function(e){
 
 /*----------------------------------------------------------*/
 
-function servicesOpacityText(){
-let servicesText = document.querySelectorAll(".services__text p");
-for (let i = 0; i < servicesText.length; i++){
-    let a = 1 / (i + 0.5);
-    servicesText[i].style.opacity = a;
-};
-}
-servicesOpacityText();
-let readMoreButton = document.querySelector(".services__more");
-let readLessButton = document.querySelector(".services__less");
-readMoreButton.addEventListener("click", readMore);
-readMoreButton.addEventListener("click", () => {document.querySelector(".services__text").style.height = "500px"});
-readLessButton.addEventListener("click", readMore);
-readLessButton.addEventListener("click", servicesOpacityText);
-readLessButton.addEventListener("click", () => {document.querySelector(".services__text").style.height = "297px"});
-function readMore(){
-    let services = document.querySelectorAll(".services p");
-    services.forEach(element => {
-        element.style.opacity = 1;
-    });
-    readMoreButton.classList.toggle("_active");
-    readLessButton.classList.toggle("_active");
-}
 
